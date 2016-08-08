2213a1aae3fd438e2b84de2795c37ccb2bf1be1b
ktsn <ktsn55@gmail.com>
2016-08-08 19:23:09 +0900

    Sync router and store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import { sync } from 'vuex-router-sync'
+
 import App from './App'
 import router from './router'
 import store from './store'
+
+sync(store, router)
 
 new Vue({
   el: '#app',
