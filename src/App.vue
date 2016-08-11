ee989c8198a22e268345ef8e64f251485bcd453a
ktsn <ktsn55@gmail.com>
2016-08-11 17:46:49 +0900

    Add reset style

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,61 @@
   </div>
 </template>
 
-<script>
-export default {
-}
-</script>
-
 <style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.2rem;
+  font-family: sans-serif;
+  line-height: 1.3;
+}
+
+body, h1, h2, h3, h4, h5, h6, p, ul, ol, li, dl, dt, dd, pre, fieldset {
+  margin: 0;
+  padding: 0;
+}
+
+fieldset {
+  border: none;
+}
+
+h1, h2, h3, h4, h5, h6, input, button, select, textarea {
+  font-size: inherit;
+}
+
+input[type="button"], input[type="submit"] {
+  -webkit-appearance: button;
+}
+
+select {
+  -webkit-appearance: menulist-button;
+}
+
+textarea {
+  font-family: sans-serif;
+  line-height: 1;
+}
+
+optgroup {
+  font-style: normal;
+}
+
+ul, ol {
+  list-style: none;
+}
+
+th {
+  text-align: left;
+}
+
+button::-moz-focus-inner, input::-moz-focus-inner {
+  border: 0;
+  padding: 0;
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,19 @@
 <template>
-  <h1>{{ title }}</h1>
+  <article>
+    <h1>{{ title }}</h1>
+    <style-test></style-test>
+  </article>
 </template>
 
 <script>
+import StyleTest from './StyleTest'
+
 export default {
   data: function () {
     return {
       title: 'Hello Vue.js!'
     }
-  }
+  },
+  components: { StyleTest }
 }
 </script>
-
-<style>
-</style>
