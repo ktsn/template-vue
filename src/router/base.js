b02ae7f175165a5651ee8fc009a505f67f7cc6f5
ktsn <ktsn55@gmail.com>
2016-08-12 14:11:37 +0900

    Separate reset css to other package

--- src/router/base.js
+++ src/router/base.js
@@ -1,9 +1,7 @@
 import Home from 'components/Home'
 import Counter from 'components/Counter'
-import StyleTest from 'components/StyleTest'
 
 export default [
   { path: '/', component: Home },
-  { path: '/counter', component: Counter },
-  { path: '/style_test', component: StyleTest }
+  { path: '/counter', component: Counter }
 ]
