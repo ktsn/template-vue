ad8594f204fd6d1eb9d5012af919567777599ad5
ktsn <ktsn55@gmail.com>
2016-08-16 21:45:47 +0900

    Output source map when testing

--- template/webpack.config.test.js
+++ template/webpack.config.test.js
@@ -4,6 +4,8 @@ const glob = require('glob')
 
 const config = require('./webpack.config.base')
 
+config.devtool = 'source-map'
+
 config.entry = glob.sync('./test/**/*.js')
 config.output = {
   path: path.resolve(__dirname, '.tmp'),
