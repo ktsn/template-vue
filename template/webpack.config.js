bee095fa1b2021acb365821d16bde4bd256ee18e
ktsn <ktsn55@gmail.com>
2016-08-16 09:26:30 +0900

    Use webpack-dashboard

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -70,6 +70,14 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new ExtractTextPlugin('main.css')
   ])
+} else {
+  const Dashboard = require('webpack-dashboard')
+  const DashboardPlugin = require('webpack-dashboard/plugin')
+  const dashboard = new Dashboard()
+
+  config.plugins.push(
+    new DashboardPlugin(dashboard.setData)
+  )
 }
 
 module.exports = config
