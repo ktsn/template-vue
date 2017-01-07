18b577210a6efacd2b39fa131a9c05d14cbf71f8
ktsn <ktsn55@gmail.com>
2017-01-08 03:45:31 +0900

    Fix scripts

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -7,7 +7,7 @@ const config = require('./webpack.config.base').config
 
 config.devtool = 'source-map'
 
-config.entry = glob.sync('./test/**/*.js')
+config.entry = ['es6-promise/auto'].concat(glob.sync('./test/**/*.js'))
 config.output = {
   path: path.resolve(__dirname, '.tmp'),
   filename: 'test.js'
